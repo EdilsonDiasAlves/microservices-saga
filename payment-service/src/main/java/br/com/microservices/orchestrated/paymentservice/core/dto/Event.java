6ea812378fe365c3eb00ad067ec10ad01758ffcc
Edilson Moizinho <edilsondiasalves@outlook.com>
2024-09-30 10:46:05 -0300

    payment-service - Creating logic to make a successful payment

--- payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/dto/Event.java
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/dto/Event.java
@@ -7,7 +7,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Data
 @Builder
@@ -23,4 +26,11 @@ public class Event {
     private SagaStatusEnum status;
     private List<History> eventHistory;
     private LocalDateTime createdAt;
+
+    public void addToHistory(History history) {
+        if (isEmpty(eventHistory)) {
+            eventHistory = new ArrayList<>();
+        }
+        eventHistory.add(history);
+    }
 }
--- payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/service/PaymentService.java
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/service/PaymentService.java
@@ -2,7 +2,9 @@ package br.com.microservices.orchestrated.paymentservice.core.service;
 
 import br.com.microservices.orchestrated.paymentservice.config.exception.ValidationException;
 import br.com.microservices.orchestrated.paymentservice.core.dto.Event;
+import br.com.microservices.orchestrated.paymentservice.core.dto.History;
 import br.com.microservices.orchestrated.paymentservice.core.dto.OrderProducts;
+import br.com.microservices.orchestrated.paymentservice.core.enums.PaymentStatusEnum;
 import br.com.microservices.orchestrated.paymentservice.core.model.Payment;
 import br.com.microservices.orchestrated.paymentservice.core.producer.KafkaProducer;
 import br.com.microservices.orchestrated.paymentservice.core.repository.PaymentRepository;
@@ -11,6 +13,10 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
+import static br.com.microservices.orchestrated.paymentservice.core.enums.SagaStatusEnum.SUCCESS;
+
 @Slf4j
 @AllArgsConstructor
 @Service
@@ -18,6 +24,7 @@ public class PaymentService {
 
     private static final String CURRENT_SOURCE = "PAYMENT_SERVICE";
     private static final Double REDUCE_SUM_VALUE = 0.0;
+    private static final Double MIN_AMOUNT_VALUE = 0.1;
 
     private final JsonUtil jsonUtil;
     private final KafkaProducer producer;
@@ -27,6 +34,10 @@ public class PaymentService {
         try {
             checkCurrentValidation(event);
             createPendingPayment(event);
+            var payment = findByOrderIdAndTransactionId(event);
+            validateAmount(payment.getTotalAmount());
+            changePaymentToSuccess(payment);
+            handleSuccess(event);
         } catch (Exception ex) {
             log.error("Error trying to realize payment");
         }
@@ -76,6 +87,42 @@ public class PaymentService {
     private void setEventAmountItems(Event event, Payment payment) {
         event.getPayload().setTotalAmount(payment.getTotalAmount());
         event.getPayload().setTotalItems(payment.getTotalItems());
+    }
+
+    private void validateAmount(double amount) {
+        if (amount < MIN_AMOUNT_VALUE) {
+            throw new ValidationException("The minimum amount available is " + MIN_AMOUNT_VALUE);
+        }
+    }
+
+    private void changePaymentToSuccess(Payment payment) {
+        payment.setStatus(PaymentStatusEnum.SUCCESS);
+        save(payment);
+    }
+
+    private void handleSuccess(Event event) {
+        event.setStatus(SUCCESS);
+        event.setSource(CURRENT_SOURCE);
+        addHistory(event, "Payment realized successfully!");
+    }
+
+    private void addHistory(Event event, String message) {
+        var history = History
+            .builder()
+            .source(event.getSource())
+            .status(event.getStatus())
+            .message(message)
+            .createdAt(LocalDateTime.now())
+            .build();
+        event.addToHistory(history);
+    }
+
+    private Payment findByOrderIdAndTransactionId(Event event) {
+        return paymentRepository
+            .findByOrderIdAndTransactionId(
+                event.getPayload().getId(),
+                event.getTransactionId())
+            .orElseThrow(() -> new ValidationException("Payment not found by orderId and transactionId"));
     }
 
     private void save(Payment payment) {
