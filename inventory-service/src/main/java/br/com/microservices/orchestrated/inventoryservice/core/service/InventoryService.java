6b3a2c32bae1b435cba17a9c8e8c4d8beaa7a420
Edilson Moizinho <edilsondiasalves@outlook.com>
2024-11-19 14:23:03 -0300

    inventory-service - Creating logic to update inventory

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
@@ -2,6 +2,8 @@ package br.com.microservices.orchestrated.inventoryservice.core.service;
 
 import br.com.microservices.orchestrated.inventoryservice.config.exception.ValidationException;
 import br.com.microservices.orchestrated.inventoryservice.core.dto.Event;
+import br.com.microservices.orchestrated.inventoryservice.core.dto.History;
+import br.com.microservices.orchestrated.inventoryservice.core.dto.Order;
 import br.com.microservices.orchestrated.inventoryservice.core.dto.OrderProducts;
 import br.com.microservices.orchestrated.inventoryservice.core.model.Inventory;
 import br.com.microservices.orchestrated.inventoryservice.core.model.OrderInventory;
@@ -12,6 +14,10 @@ import br.com.microservices.orchestrated.inventoryservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+import static br.com.microservices.orchestrated.inventoryservice.core.enums.SagaStatusEnum.SUCCESS;
 
 @Slf4j
 @AllArgsConstructor
@@ -29,6 +35,8 @@ public class InventoryService {
         try {
             checkCurrentValidation(event);
             createOrderInventory(event);
+            updateInventory(event.getPayload());
+            handleSuccess(event);
         } catch (Exception ex) {
             log.error("Error trying to update inventory", ex);
         }
@@ -64,6 +72,40 @@ public class InventoryService {
             .orderId(event.getPayload().getId())
             .transactionId(event.getTransactionId())
             .build();
+    }
+
+    private void updateInventory(Order order) {
+        order .getProducts()
+            .forEach(product -> {
+                var inventory = findInventoryByProductCode(product.getProduct().getCode());
+                checkInventory(inventory.getAvailable(), product.getQuantity());
+                inventory.setAvailable(inventory.getAvailable() - product.getQuantity());
+                inventoryRepository.save(inventory);
+            }
+        );
+    }
+
+    private void checkInventory(int available, int orderQuantity) {
+        if (orderQuantity > available) {
+            throw new ValidationException("Product is out of stock!");
+        }
+    }
+
+    private void handleSuccess(Event event) {
+        event.setStatus(SUCCESS);
+        event.setSource(CURRENT_SOURCE);
+        addHistory(event, "Inventory updated successfully!");
+    }
+
+    private void addHistory(Event event, String message) {
+        var history = History
+            .builder()
+            .source(event.getSource())
+            .status(event.getStatus())
+            .message(message)
+            .createdAt(LocalDateTime.now())
+            .build();
+        event.addToHistory(history);
     }
 
     private Inventory findInventoryByProductCode(String productCode) {
